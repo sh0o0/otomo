0865bca215fd174ed6f38c659060df55b6615cc5
nakar0 <shoutaro425@gmail.com>
2023-07-26 17:03:52 +0700

    :+1: [api] Impl NewMessageWithOtomoRepository

--- api/internal/app/interface/gateway/repository/chat_with_otomo.go
+++ api/internal/app/interface/gateway/repository/chat_with_otomo.go
@@ -4,11 +4,22 @@ import (
 	"context"
 	"otomo/internal/app/domain/entity/message"
 	"otomo/internal/app/domain/gateway/repo"
+
+	"cloud.google.com/go/firestore"
 )
 
 var _ repo.MessageWithOtomoRepository = (*MessageWithOtomoRepository)(nil)
 
 type MessageWithOtomoRepository struct {
+	fsClient *firestore.Client
+}
+
+func NewMessageWithOtomoRepository(
+	fsClient *firestore.Client,
+) *MessageWithOtomoRepository {
+	return &MessageWithOtomoRepository{
+		fsClient: fsClient,
+	}
 }
 
 func (r *MessageWithOtomoRepository) Add(
