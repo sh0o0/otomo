728c64a252d804813ddc4508e1b6c8901502a5a7
nakar0 <shoutaro425@gmail.com>
2023-08-04 17:12:58 +0700

    :+1: [api] Added domain dir

--- api/internal/app/interface/controller/chat_controller.go
+++ api/internal/app/interface/controller/chat_controller.go
@@ -10,5 +10,9 @@ func (c *ChatController) SendMessage(
 	_ *grpcgen.ChatService_SendMessageRequest,
 	_ grpcgen.ChatService_SendMessageServer,
 ) error {
+	// STEP: Add a message to the repo
+	// STEP: make a reply
+	// STEP: response
+	// STEP: Add a message to the repo
 	panic("not implemented") // TODO: Implement
 }
