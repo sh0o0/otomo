038df18ebcac9ddf78fd7f15fb38c1376fa405b3
nakar0 <shoutaro425@gmail.com>
2023-08-04 17:49:20 +0700

    :construction: [api] Impl temporarily SendMessage

--- api/go.mod
+++ api/go.mod
@@ -3,12 +3,15 @@ module otomo
 go 1.20
 
 require (
+	github.com/dlclark/regexp2 v1.10.0 // indirect
 	github.com/envoyproxy/protoc-gen-validate v1.0.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/iancoleman/strcase v0.2.0 // indirect
 	github.com/lyft/protoc-gen-star/v2 v2.0.3 // indirect
+	github.com/pkoukk/tiktoken-go v0.1.5 // indirect
 	github.com/spf13/afero v1.3.3 // indirect
+	github.com/tmc/langchaingo v0.0.0-20230802030916-271e9bd7e7c5 // indirect
 	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
 	golang.org/x/mod v0.11.0 // indirect
 	golang.org/x/net v0.11.0 // indirect
--- api/internal/app/interface/controller/chat_controller.go
+++ api/internal/app/interface/controller/chat_controller.go
@@ -1,18 +1,41 @@
 package controller
 
-import "otomo/internal/app/interface/controller/grpc/grpcgen"
+import (
+	"context"
+	"fmt"
+	"otomo/internal/app/interface/controller/grpc/grpcgen"
+
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/openai"
+	"github.com/tmc/langchaingo/schema"
+)
 
 var _ grpcgen.ChatServiceServer = (*ChatController)(nil)
 
-type ChatController struct{}
+type ChatController struct {
+}
 
 func (c *ChatController) SendMessage(
-	_ *grpcgen.ChatService_SendMessageRequest,
-	_ grpcgen.ChatService_SendMessageServer,
+	req *grpcgen.ChatService_SendMessageRequest,
+	stream grpcgen.ChatService_SendMessageServer,
 ) error {
 	// STEP: Add a message to the repo
 	// STEP: make a reply
 	// STEP: response
 	// STEP: Add a message to the repo
-	panic("not implemented") // TODO: Implement
+
+	llm, err := openai.NewChat()
+	if err != nil {
+		return err
+	}
+	completion, err := llm.Call(stream.Context(), []schema.ChatMessage{
+		schema.SystemChatMessage{Content: "Hello, I am a friendly chatbot. I love to talk about movies, books and music. Answer in long form yaml."},
+		schema.HumanChatMessage{Content: "What would be a good company name a company that makes colorful socks?"},
+	}, llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
+		fmt.Print(string(chunk))
+		return nil
+	}),
+	)
+	fmt.Println(completion)
+	return err
 }
