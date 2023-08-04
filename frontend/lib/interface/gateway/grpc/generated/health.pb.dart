8fb5d08463e329d3c777fe2d607f0d6b47707bf2
nakar0 <shoutaro425@gmail.com>
2023-08-04 16:41:57 +0700

    :+1: [grpc] Prepared generating grpc code

---
+++ frontend/lib/interface/gateway/grpc/generated/health.pb.dart
@@ -0,0 +1,60 @@
+//
+//  Generated code. Do not modify.
+//  source: health.proto
+//
+// @dart = 2.12
+
+// ignore_for_file: annotate_overrides, camel_case_types
+// ignore_for_file: constant_identifier_names, library_prefixes
+// ignore_for_file: non_constant_identifier_names, prefer_final_fields
+// ignore_for_file: unnecessary_import, unnecessary_this, unused_import
+
+import 'dart:core' as $core;
+
+import 'package:protobuf/protobuf.dart' as $pb;
+
+class HealthCheckResponse extends $pb.GeneratedMessage {
+  factory HealthCheckResponse() => create();
+  HealthCheckResponse._() : super();
+  factory HealthCheckResponse.fromBuffer($core.List<$core.int> i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromBuffer(i, r);
+  factory HealthCheckResponse.fromJson($core.String i, [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) => create()..mergeFromJson(i, r);
+
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(_omitMessageNames ? '' : 'HealthCheckResponse', createEmptyInstance: create)
+    ..aOB(1, _omitFieldNames ? '' : 'health')
+    ..hasRequiredFields = false
+  ;
+
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
+  'Will be removed in next major version')
+  HealthCheckResponse clone() => HealthCheckResponse()..mergeFromMessage(this);
+  @$core.Deprecated(
+  'Using this can add significant overhead to your binary. '
+  'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
+  'Will be removed in next major version')
+  HealthCheckResponse copyWith(void Function(HealthCheckResponse) updates) => super.copyWith((message) => updates(message as HealthCheckResponse)) as HealthCheckResponse;
+
+  $pb.BuilderInfo get info_ => _i;
+
+  @$core.pragma('dart2js:noInline')
+  static HealthCheckResponse create() => HealthCheckResponse._();
+  HealthCheckResponse createEmptyInstance() => create();
+  static $pb.PbList<HealthCheckResponse> createRepeated() => $pb.PbList<HealthCheckResponse>();
+  @$core.pragma('dart2js:noInline')
+  static HealthCheckResponse getDefault() => _defaultInstance ??= $pb.GeneratedMessage.$_defaultFor<HealthCheckResponse>(create);
+  static HealthCheckResponse? _defaultInstance;
+
+  @$pb.TagNumber(1)
+  $core.bool get health => $_getBF(0);
+  @$pb.TagNumber(1)
+  set health($core.bool v) { $_setBool(0, v); }
+  @$pb.TagNumber(1)
+  $core.bool hasHealth() => $_has(0);
+  @$pb.TagNumber(1)
+  void clearHealth() => clearField(1);
+}
+
+
+const _omitFieldNames = $core.bool.fromEnvironment('protobuf.omit_field_names');
+const _omitMessageNames = $core.bool.fromEnvironment('protobuf.omit_message_names');
---
+++ grpc/scripts/generate.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+echo "Start generate"
+
+cd $(dirname $0)
+
+PROTO_DIR="/usr/local/include"
+PROTOS_DIR=../protos
+PROTO_FILES=$PROTOS_DIR/*.proto
+DOCS_DIR=../../docs
+API_DIR=../../api
+FRONTEND_DIR=../../frontend
+# FIREBASE_DIR=../../firebase
+
+EXCLUDED_VALIDATE_PROTOS_DIR="../excluded_validate_protos"
+EXCLUDED_VALIDATE_PROTO_FILES="../excluded_validate_protos/*.proto"
+
+setup() {
+    cp -r $PROTOS_DIR $EXCLUDED_VALIDATE_PROTOS_DIR
+
+    for dir in $(find "${EXCLUDED_VALIDATE_PROTOS_DIR}" -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq); do
+        sed -i -E "s/ \[(validate.rules).*\];/;/g" $(find "${dir}" -name '*.proto')
+        sed -i -E "/validate.proto/d" $(find "${dir}" -name '*.proto')
+    done
+}
+
+cleanup() {
+    rm -r $EXCLUDED_VALIDATE_PROTOS_DIR
+}
+
+setup
+
+### Go & validator & docs
+protoc \
+    -I $PROTOS_DIR \
+    -I ${GOPATH}/src \
+    -I ${GOPATH}/src/github.com/envoyproxy/protoc-gen-validate \
+    -I ${PROTO_DIR} \
+    --go_out=$API_DIR/internal/app/interface/controller/grpc/grpcgen/ \
+    --go-grpc_opt=require_unimplemented_servers=false \
+    --go-grpc_out=$API_DIR/internal/app/interface/controller/grpc/grpcgen/ \
+    --validate_out="lang=go:$API_DIR/internal/app/interface/controller/grpc/grpcgen/" \
+    --doc_out=html,grpc.html:$DOCS_DIR/grpc/ \
+    $PROTO_FILES
+
+### Dart
+protoc \
+    -I $PROTOS_DIR \
+    -I ${GOPATH}/src \
+    -I ${GOPATH}/src/github.com/envoyproxy/protoc-gen-validate \
+    -I ${PROTO_DIR} \
+    --dart_out=grpc:$FRONTEND_DIR/lib/interface/gateway/grpc/generated/ \
+    ${PROTO_DIR}/google/protobuf/timestamp.proto \
+    ${PROTO_DIR}/google/protobuf/empty.proto \
+    $PROTO_FILES
+
+### TS
+# Reference: https://qiita.com/ohs30359-nobuhara/items/f11857d5d3d9dbc6637b
+# PLUGIN_TS=$FIREBASE_DIR/functions/node_modules/.bin/protoc-gen-ts
+# PLUGIN_GRPC=$FIREBASE_DIR/functions/node_modules/.bin/grpc_tools_node_protoc_plugin
+# DIST_DIR=$FIREBASE_DIR/functions/src/protos
+
+# protoc \
+#     -I $EXCLUDED_VALIDATE_PROTOS_DIR \
+#     -I ${GOPATH}/src \
+#     -I ${PROTO_DIR} \
+#     --js_out=import_style=commonjs,binary:"${DIST_DIR}"/ \
+#     --ts_out=import_style=commonjs,binary:"${DIST_DIR}"/ \
+#     --grpc_out="${DIST_DIR}"/ \
+#     --plugin=protoc-gen-grpc="${PLUGIN_GRPC}" \
+#     --plugin=protoc-gen-ts="${PLUGIN_TS}" \
+#     $EXCLUDED_VALIDATE_PROTO_FILES
+
+echo "Finish generate"
+
+cleanup
