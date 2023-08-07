bbc058c8ab15c243d8b04ba35ee0033bc1063d8c
nakar0 <shoutaro425@gmail.com>
2023-08-07 15:25:38 +0700

    :construction: [api] Impl path and utils in repository

---
+++ api/internal/app/interface/gateway/repository/path.go
@@ -0,0 +1,28 @@
+package repository
+
+import "strings"
+
+const (
+	versionColName  string = "versions"
+	version1DocName string = "1"
+
+	userColName    string = "users"
+	messageColName string = "messages"
+)
+
+var (
+	//  /versions/1
+	v1DocPath string = strings.Join([]string{versionColName, version1DocName}, "/")
+	//  /versions/1/users
+	userColPath string = strings.Join([]string{v1DocPath, userColName}, "/")
+)
+
+// /versions/1/users/{userID}/messages
+func getMessagesColPath(userID string) string {
+	return strings.Join([]string{userColPath, userID, messageColName}, "/")
+}
+
+// /versions/1/users/{userID}/messages/{msgID}
+func getMessageDocPath(userID string, msgID string) string {
+	return strings.Join([]string{getMessagesColPath(userID), msgID}, "/")
+}
---
+++ api/internal/app/interface/gateway/repository/utils.go
@@ -0,0 +1,24 @@
+package repository
+
+import (
+	"otomo/pkg/errs"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func ifCodesNotFoundReturnErrsNotFound(
+	err error,
+	domain errs.Domain,
+	field errs.Field,
+) error {
+	if status.Code(err) == codes.NotFound {
+		return &errs.Error{
+			Message: err.Error(),
+			Cause:   errs.CauseNotFound,
+			Domain:  domain,
+			Field:   field,
+		}
+	}
+	return err
+}
