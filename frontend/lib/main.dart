9fe1343734a197ac0c4585707ac9b38685398cb6
nakar0 <shoutaro425@gmail.com>
2023-08-16 16:18:47 +0800

    :+1: [frontend] Configurated firebase

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:otomo/firebase_options.dart';
 import 'package:otomo/views/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const ProviderScope(child: App()));
 }
