51d232c182fd11d45576e703227658e5c09b5bd2
nakar0 <shoutaro425@gmail.com>
2023-08-18 17:11:18 +0800

    :+1: [FE] Deleted sign in with email feature

--- frontend/lib/views/pages/sign_in/sign_in.dart
+++ frontend/lib/views/pages/sign_in/sign_in.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:otomo/viewmodels/sign_in.dart';
 import 'package:otomo/views/bases/layouts/safe_area_layout.dart';
 import 'package:otomo/views/bases/layouts/side_space_layout.dart';
 import 'package:otomo/views/bases/screens/app_scaffold.dart';
 import 'package:otomo/views/bases/spaces/spaces.dart';
-import 'package:otomo/views/bases/texts/tappable_text.dart';
 import 'package:otomo/views/cases/buttons/sign_in_button.dart';
-import 'package:otomo/views/router.dart';
 
 class SignInPage extends HookConsumerWidget {
   const SignInPage({super.key});
@@ -31,14 +28,14 @@ class SignInPage extends HookConsumerWidget {
                   onPressed: () =>
                       ref.read(signInProvider.notifier).signInWithGoogle(),
                 ),
-                Spaces.h24,
-                TappableText(
-                  'Continue with email',
-                  textStyle: theme.textTheme.bodyLarge?.copyWith(
-                    decoration: TextDecoration.underline,
-                  ),
-                  onTap: () => context.push(Routes.signInWithEmailLink),
-                ),
+                // Spaces.h24,
+                // TappableText(
+                //   'Continue with email',
+                //   textStyle: theme.textTheme.bodyLarge?.copyWith(
+                //     decoration: TextDecoration.underline,
+                //   ),
+                //   onTap: () => context.push(Routes.signInWithEmailLink),
+                // ),
               ],
             ),
           ),
