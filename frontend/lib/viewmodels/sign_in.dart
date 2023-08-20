d5c375baca4254ab18145e99cd85c35bdc712833
nakar0 <shoutaro425@gmail.com>
2023-08-20 14:15:36 +0800

    :+1: [FE] Made controllers dir

--- frontend/lib/viewmodels/sign_in.dart
+++ frontend/lib/viewmodels/sign_in.dart
@@ -1,25 +1,15 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:otomo/controllers/sign_in.dart';
 import 'package:otomo/injection.dart';
 
 final signInProvider = StateNotifierProvider((ref) {
-  return SignInInNotifier();
+  return SignInInNotifier(getIt<SignInController>());
 });
 
 class SignInInNotifier extends StateNotifier<void> {
-  SignInInNotifier() : super(null);
+  SignInInNotifier(this._controller) : super(null);
 
-  final _googleSignIn = getIt<GoogleSignIn>();
+  final SignInController _controller;
 
-  Future<void> signInWithGoogle() async {
-    final googleUser = await _googleSignIn.signIn();
-    final googleAuth = await googleUser?.authentication;
-
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth?.accessToken,
-      idToken: googleAuth?.idToken,
-    );
-    await getIt<FirebaseAuth>().signInWithCredential(credential);
-  }
+  Future<void> signInWithGoogle() => _controller.signInWithGoogle();
 }
