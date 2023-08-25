80690310a7a4bf72bfca1f97bf61d12a7efeb798
nakar0 <shoutaro425@gmail.com>
2023-08-25 16:25:28 +0800

    :+1: [FE] Added log to main func

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:otomo/configs/app_config.dart';
 import 'package:otomo/configs/injection.dart';
 import 'package:otomo/firebase_options.dart';
+import 'package:otomo/tools/logger.dart';
 import 'package:otomo/views/app.dart';
 
 void main() async {
+  logger.info(appConfig.toString());
   await setup();
   runApp(const ProviderScope(child: App()));
 }
