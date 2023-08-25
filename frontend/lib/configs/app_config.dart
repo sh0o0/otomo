3ab50d4a5c5577662c3829967ee2d3d2beefb681
nakar0 <shoutaro425@gmail.com>
2023-08-25 15:55:53 +0800

    :+1: [FE] Added app config

---
+++ frontend/lib/configs/app_config.dart
@@ -0,0 +1,47 @@
+import 'package:logger/logger.dart';
+
+AppConfig appConfig = AppConfig.fromEnv();
+
+class Env {
+  Env._();
+
+  static const bool isRelease = bool.fromEnvironment('dart.vm.product');
+  static const String otomoServerHost =
+      String.fromEnvironment('OTOMO_SERVER_HOST');
+  static const int otomoServerPort = int.fromEnvironment('OTOMO_SERVER_PORT');
+  static const bool isSecureConnectionToOtomoServer =
+      bool.fromEnvironment('IS_SECURE_CONNECTION_TO_OTOMO_SERVER');
+}
+
+class AppConfig {
+  AppConfig.fromEnv()
+      : isRelease = Env.isRelease,
+        otomoServerHost = Env.otomoServerHost,
+        otomoServerPort = Env.otomoServerPort,
+        isSecureConnectionToOtomoServer = Env.isSecureConnectionToOtomoServer,
+        logLevel = Env.isRelease ? Level.info : Level.all,
+        logIncludeCallerInfo = !Env.isRelease,
+        firebaseAutomaticDataCollectionEnabled = true,
+        firebaseAutomaticResourceManagementEnabled = true;
+
+  final bool isRelease;
+  final String otomoServerHost;
+  final int otomoServerPort;
+  final bool isSecureConnectionToOtomoServer;
+  final Level logLevel;
+  final bool logIncludeCallerInfo;
+  final bool firebaseAutomaticDataCollectionEnabled;
+  final bool firebaseAutomaticResourceManagementEnabled;
+
+  @override
+  String toString() {
+    return '''isRelease: $isRelease
+otomoServerHost: $otomoServerHost
+otomoServerPort: $otomoServerPort
+logLevel: $logLevel
+logIncludeCallerInfo: $logIncludeCallerInfo
+firebaseAutomaticDataCollectionEnabled: $firebaseAutomaticDataCollectionEnabled
+firebaseAutomaticResourceManagementEnabled: $firebaseAutomaticResourceManagementEnabled
+''';
+  }
+}
