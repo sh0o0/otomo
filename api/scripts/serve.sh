e81ac07aa368783f477701f277a25530d25e703d
nakar0 <shoutaro425@gmail.com>
2023-08-26 17:14:13 +0800

    :+1: [BE] Fixed name of conf.go file

--- api/scripts/serve.sh
+++ api/scripts/serve.sh
@@ -4,10 +4,6 @@ cd $(dirname $0)
 
 direnv allow .
 
-printf '\033[32m%s\033[m\n' 'Starting Firestore Emulator...'
-
 . firestore_emulator.sh
-
-printf '\033[32m%s\033[m\n' 'Started Firestore Emulator'
 
 go run ../cmd/app/*
