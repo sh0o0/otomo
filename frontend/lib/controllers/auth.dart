4dda1ff97c23445529ff84eccf002fd6d595f688
nakar0 <shoutaro425@gmail.com>
2023-08-27 14:32:29 +0800

    :+1: [FE] Transter auth.User to models.User in controller

--- frontend/lib/controllers/auth.dart
+++ frontend/lib/controllers/auth.dart
@@ -1,13 +1,16 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:injectable/injectable.dart';
+import 'package:otomo/models/user.dart';
 
 @Injectable()
 class AuthController {
   AuthController(this._firebaseAuth);
 
-  final FirebaseAuth _firebaseAuth;
+  final auth.FirebaseAuth _firebaseAuth;
 
-  Stream<User?> authStateChanges() => _firebaseAuth.authStateChanges();
+  Stream<User?> authStateChanges() => _firebaseAuth
+      .authStateChanges()
+      .map((authUser) => authUser == null ? null : User(id: authUser.uid));
 
   Future<String?> getIdToken() async {
     return _firebaseAuth.currentUser?.getIdToken();
