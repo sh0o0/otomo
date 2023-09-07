15d38c6916b692839cffdedb3a1e9a76631c081a
nakar0 <shoutaro425@gmail.com>
2023-09-07 15:05:19 +0900

    :+1: [BE] Added Mapbox view

--- frontend/lib/views/pages/account/account.dart
+++ frontend/lib/views/pages/account/account.dart
@@ -1,10 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:otomo/configs/injection.dart';
+import 'package:otomo/views/bases/buttons/rounded_filled_button.dart';
 
 class Account extends StatelessWidget {
   const Account({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: RoundedFilledButton.large(
+        onPressed: getIt<FirebaseAuth>().signOut,
+        child: const Text('Sign out'),
+      ),
+    );
   }
 }
--- frontend/lib/views/pages/map/map.dart
+++ frontend/lib/views/pages/map/map.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+import 'package:otomo/configs/app_config.dart';
 
 class AppMap extends StatelessWidget {
   const AppMap({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MapWidget(
+      resourceOptions: ResourceOptions(
+        accessToken: appConfig.mapboxPublicToken,
+      ),
+    );
   }
 }
