152b757ba690c2e4df6f7f261bb0bbd155edcfc5
nakar0 <shoutaro425@gmail.com>
2023-09-07 14:24:27 +0900

    :+1: [BE] Added mapbox

--- frontend/lib/configs/app_config.dart
+++ frontend/lib/configs/app_config.dart
@@ -2,8 +2,8 @@ import 'package:logger/logger.dart';
 
 AppConfig appConfig = AppConfig.fromEnv();
 
-class Env {
-  Env._();
+class _Env {
+  _Env._();
 
   static const bool isRelease = bool.fromEnvironment('dart.vm.product');
   static const String otomoServerHost =
@@ -11,20 +11,24 @@ class Env {
   static const int otomoServerPort = int.fromEnvironment('OTOMO_SERVER_PORT');
   static const bool isSecureConnectionToOtomoServer =
       bool.fromEnvironment('IS_SECURE_CONNECTION_TO_OTOMO_SERVER');
+  static const String mapboxPublicToken =
+      String.fromEnvironment('MAPBOX_PUBLIC_TOKEN');
 }
 
 class AppConfig {
   AppConfig.fromEnv()
-      : isRelease = Env.isRelease,
-        otomoServerHost = Env.otomoServerHost,
-        otomoServerPort = Env.otomoServerPort,
-        isSecureConnectionToOtomoServer = Env.isSecureConnectionToOtomoServer,
-        logLevel = Env.isRelease ? Level.info : Level.all;
+      : isRelease = _Env.isRelease,
+        otomoServerHost = _Env.otomoServerHost,
+        otomoServerPort = _Env.otomoServerPort,
+        isSecureConnectionToOtomoServer = _Env.isSecureConnectionToOtomoServer,
+        mapboxPublicToken = _Env.mapboxPublicToken,
+        logLevel = _Env.isRelease ? Level.info : Level.all;
 
   final bool isRelease;
   final String otomoServerHost;
   final int otomoServerPort;
   final bool isSecureConnectionToOtomoServer;
+  final String mapboxPublicToken;
   final Level logLevel;
 
   @override
