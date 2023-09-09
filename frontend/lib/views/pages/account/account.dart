1bb14ffa5440bf9604bfbe073c6c60f28ea34a78
nakar0 <shoutaro425@gmail.com>
2023-09-09 21:48:14 +0900

    :+1: [FE] Tried impl draggable sheet

--- frontend/lib/views/pages/account/account.dart
+++ frontend/lib/views/pages/account/account.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:otomo/configs/injection.dart';
 import 'package:otomo/views/bases/buttons/rounded_filled_button.dart';
+import 'package:otomo/views/bases/spaces/spaces.dart';
+import 'package:otomo/views/pages/samples/sample_map_chat.dart';
 
 class Account extends StatelessWidget {
   const Account({super.key});
@@ -9,9 +11,20 @@ class Account extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: RoundedFilledButton.large(
-        onPressed: getIt<FirebaseAuth>().signOut,
-        child: const Text('Sign out'),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          RoundedFilledButton.large(
+            onPressed: getIt<FirebaseAuth>().signOut,
+            child: const Text('Sign out'),
+          ),
+          Spaces.h8,
+          RoundedFilledButton.large(
+            onPressed: () => Navigator.of(context)
+                .push(MaterialPageRoute(builder: (_) => const SampleMapChat())),
+            child: const Text('Sample Map Chat'),
+          ),
+        ],
       ),
     );
   }
--- frontend/lib/views/pages/samples/cases/sample_chat.dart
+++ frontend/lib/views/pages/samples/cases/sample_chat.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:otomo/tools/faker.dart';
+import 'package:otomo/tools/logger.dart';
 import 'package:otomo/tools/uuid.dart';
 
 class SampleChat extends StatefulWidget {
-  const SampleChat({super.key});
+  const SampleChat({
+    super.key,
+    this.theme,
+  });
+
+  final ChatTheme? theme;
 
   @override
   State<SampleChat> createState() => _SampleChatState();
@@ -18,6 +24,15 @@ class _SampleChatState extends State<SampleChat> {
   @override
   Widget build(BuildContext context) {
     return Chat(
+      theme: widget.theme ?? const DefaultChatTheme(),
+      onSendPressed: (m) {},
+      user: user,
+      customBottomWidget: SingleChildScrollView(
+        child: Input(
+          onSendPressed: (m) {},
+        ),
+      ),
+      onEndReached: () async => logger.debug('Reached end'),
       messages: [
         types.TextMessage(
           author: user,
@@ -44,9 +59,32 @@ class _SampleChatState extends State<SampleChat> {
           id: uuid(),
           text: defaultFaker.lorem.sentence(),
         ),
+        types.TextMessage(
+          author: other,
+          id: uuid(),
+          text: defaultFaker.lorem.sentence(),
+        ),
+        types.TextMessage(
+          author: other,
+          id: uuid(),
+          text: defaultFaker.lorem.sentence(),
+        ),
+        types.TextMessage(
+          author: other,
+          id: uuid(),
+          text: defaultFaker.lorem.sentence(),
+        ),
+        types.TextMessage(
+          author: other,
+          id: uuid(),
+          text: defaultFaker.lorem.sentence(),
+        ),
+        types.TextMessage(
+          author: other,
+          id: uuid(),
+          text: defaultFaker.lorem.sentence(),
+        ),
       ],
-      onSendPressed: (m) {},
-      user: user,
     );
   }
 }
