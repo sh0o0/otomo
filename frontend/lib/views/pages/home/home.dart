6424972e586cac1980353aecee1d39e784618880
nakar0 <shoutaro425@gmail.com>
2023-09-13 12:09:52 +0900

    :+1: [FE] Added navigation

--- frontend/lib/views/pages/home/home.dart
+++ frontend/lib/views/pages/home/home.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:otomo/tools/logger.dart';
 import 'package:otomo/views/bases/text_fields/unfocus_when_tap.dart';
 import 'package:otomo/views/cases/chat/chat_bottom_sheet_bar.dart';
 import 'package:otomo/views/cases/home/home_with_draggable_scrollable_bottom_sheet.dart';
 import 'package:otomo/views/cases/home/swipe_selection_floating_action_button.dart';
 import 'package:otomo/views/pages/home/cases/home_chat.dart';
 import 'package:otomo/views/pages/map/map.dart';
+import 'package:otomo/views/router.dart';
 
 class HomePage extends StatefulHookConsumerWidget {
   const HomePage({super.key});
@@ -46,9 +46,7 @@ class _HomePageState extends ConsumerState<HomePage> {
         primaryButtonIcon: Icons.chat,
         topButton: SwipeSelectionButtonData(
           icon: Icons.account_circle,
-          onSelected: () {
-            logger.debug('selected top button');
-          },
+          onSelected: () => ref.read(routerProvider).push(Routes.account),
         ),
         onPrimaryButtonPressed: () {
           _sheetController?.animateTo(
--- frontend/lib/views/router.dart
+++ frontend/lib/views/router.dart
@@ -2,19 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:otomo/view_models/user.dart';
+import 'package:otomo/views/pages/account/account.dart';
 import 'package:otomo/views/pages/home/home.dart';
 import 'package:otomo/views/pages/sign_in/sign_in.dart';
 import 'package:otomo/views/pages/sign_in_with_email_link/sign_in_with_email_link.dart';
 
 abstract class Routes {
-  static const home = '/home';
   static const signIn = '/sign_in';
   static const signInWithEmailLink = '/sign_in/email';
+  static const home = '/home';
+  static const account = '/account';
 }
 
 final _key = GlobalKey<NavigatorState>();
 
-final List<GoRoute> _signInPages = [
+final List<GoRoute> _notSignedInPages = [
   GoRoute(
     path: Routes.signIn,
     builder: (context, state) => const SignInPage(),
@@ -25,13 +27,24 @@ final List<GoRoute> _signInPages = [
   ),
 ];
 
+final List<GoRoute> _signedInPages = [
+  GoRoute(
+    path: Routes.home,
+    builder: (context, state) => const HomePage(),
+  ),
+  GoRoute(
+    path: Routes.account,
+    builder: (context, state) => const AccountPage(),
+  )
+];
+
 final routerProvider = Provider((ref) {
   final user = ref.watch(userProvider);
   if (user == null) {
     return GoRouter(
       navigatorKey: _key,
       initialLocation: Routes.signIn,
-      routes: _signInPages,
+      routes: _notSignedInPages,
     );
   }
 
@@ -39,11 +52,8 @@ final routerProvider = Provider((ref) {
     navigatorKey: _key,
     initialLocation: Routes.home,
     routes: [
-      ..._signInPages,
-      GoRoute(
-        path: Routes.home,
-        builder: (context, state) => const HomePage(),
-      ),
+      ..._notSignedInPages,
+      ..._signedInPages,
     ],
   );
 });
