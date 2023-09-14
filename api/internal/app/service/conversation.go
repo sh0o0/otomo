65f9d5f567c0aa08c93ab2614d70e56e44174e9b
nakar0 <shoutaro425@gmail.com>
2023-09-14 21:33:15 +0900

    :+1: [BE] Added not implemented ConversationService

--- api/internal/app/service/conversation.go
+++ api/internal/app/service/conversation.go
@@ -1,3 +1,25 @@
 package service
 
+import (
+	"context"
+	"otomo/internal/app/model"
+)
+
+var _ model.Converser = (*ConversationService)(nil)
+
 type ConversationService struct{}
+
+func (cs *ConversationService) Respond(
+	ctx context.Context,
+	msg *model.Message,
+	memory *model.Memory,
+) (*model.Message, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (cs *ConversationService) Message(
+	ctx context.Context,
+	memory *model.Memory,
+) (*model.Message, error) {
+	panic("not implemented") // TODO: Implement
+}
