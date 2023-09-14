48ed021c1911f040532eb57a6f043beb741cc4e4
nakar0 <shoutaro425@gmail.com>
2023-09-14 22:16:54 +0900

    :+1: [BE] Impl ConversationService.Message

--- api/internal/app/service/conversation.go
+++ api/internal/app/service/conversation.go
@@ -11,25 +11,29 @@ import (
 )
 
 const (
-	respondPrompt = `The following is a friendly conversation between a user and an AI called Otomo. Otomo is talkative and provides lots of specific details from its context. If Otomo does not know the answer to a question, it truthfully says it does not know.
-When Otomo mentions the name of a place, if it knows the longitude and latitude of the place, it says it in the form %[name of place]({ "type": "object", "properties": { "lat_lng": { "type": "object", "properties": { "latitude": { "type": "number" }, "longitude": { "type": "number" } } } } }). () is JSON Schema. The corresponding json is actually input. For example, Tokyo is %[Tokyo]({ "lat_lng": { "latitude": 35.6762, "longitude": 139.6503 } }).
+	otomoCommonPrompt = `The following is a friendly conversation between a user and an AI called Otomo. Otomo is talkative and provides lots of specific details from its context. If Otomo does not know the answer to a question, it truthfully says it does not know.
+When Otomo mentions the name of a place, if it knows the longitude and latitude of the place, it says it in the form %[name of place]({ "type": "object", "properties": { "lat_lng": { "type": "object", "properties": { "latitude": { "type": "number" }, "longitude": { "type": "number" } } } } }). () is JSON Schema. The corresponding json is actually input. For example, Tokyo is %[Tokyo]({ "lat_lng": { "latitude": 35.6762, "longitude": 139.6503 } }).`
+
+	respondPrompt = otomoCommonPrompt + `
 
 Current conversation:
 {{.history}}
+
 User: {{.input}}
 Otomo:`
 
-// 	messagePrompt = `The following is a friendly conversation between a user and an AI called Otomo. Otomo is talkative and provides lots of specific details from its context. If Otomo does not know the answer to a question, it truthfully says it does not know.
-// When Otomo mentions the name of a place, if it knows the longitude and latitude of the place, it says it in the form %[name of place]({ "type": "object", "properties": { "lat_lng": { "type": "object", "properties": { "latitude": { "type": "number" }, "longitude": { "type": "number" } } } } }). () is JSON Schema. The corresponding json is actually input. For example, Tokyo is %[Tokyo]({ "lat_lng": { "latitude": 35.6762, "longitude": 139.6503 } }).
+	messagePrompt = otomoCommonPrompt + `
+The conversation currently stops at your message, so please create a message that you think will be of interest to the User. You may refer to the information in the Current Conversation below, or if there is no information in the Current Conversation, compose a message on a topic of interest to the new User. Start the conversation with a greeting.
 
-// Current conversation:
-// {{.history}}
-// User: {{.input}}
-// Otomo:`
+Current conversation:
+{{.history}}
+
+Otomo:`
 )
 
 var _ model.Converser = (*ConversationService)(nil)
 
+// TODO: Add tests
 type ConversationService struct {
 	gpt        *openai.Chat
 	msgFactory *model.MessageFactory
@@ -47,7 +51,7 @@ func (cs *ConversationService) Respond(
 	memory *model.Memory,
 	listeningFunc func(ctx context.Context, chunk []byte) error,
 ) (*model.Message, error) {
-	gptMsg, err := prompts.NewHumanMessagePromptTemplate(
+	gptMsgs, err := prompts.NewHumanMessagePromptTemplate(
 		respondPrompt,
 		[]string{"history", "input"},
 	).FormatMessages(
@@ -57,7 +61,7 @@ func (cs *ConversationService) Respond(
 		return nil, err
 	}
 
-	return cs.call(ctx, gptMsg, listeningFunc)
+	return cs.call(ctx, gptMsgs, listeningFunc)
 }
 
 func (cs *ConversationService) Message(
@@ -65,7 +69,15 @@ func (cs *ConversationService) Message(
 	memory *model.Memory,
 	listeningFunc func(ctx context.Context, chunk []byte) error,
 ) (*model.Message, error) {
-	panic("not implemented")
+	gptMsgs, err := prompts.NewSystemMessagePromptTemplate(
+		messagePrompt,
+		[]string{"history"},
+	).FormatMessages(map[string]any{"history": memory.Summary})
+	if err != nil {
+		return nil, err
+	}
+
+	return cs.call(ctx, gptMsgs, listeningFunc)
 }
 
 func (cs *ConversationService) call(
