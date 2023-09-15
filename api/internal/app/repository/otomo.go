0780b3c104ab5ea14dd43fe5974ef34d694e8190
nakar0 <shoutaro425@gmail.com>
2023-09-15 11:37:26 +0900

    :+1: [BE] Added not implemented OtomoRepository

--- api/internal/app/repository/otomo.go
+++ api/internal/app/repository/otomo.go
@@ -1,1 +1,35 @@
 package repository
+
+import (
+	"context"
+	"otomo/internal/app/interfaces/repo"
+	"otomo/internal/app/model"
+
+	"cloud.google.com/go/firestore"
+)
+
+var _ repo.OtomoRepository = (*OtomoRepository)(nil)
+
+type OtomoRepository struct {
+	fsClient *firestore.Client
+}
+
+func NewOtomoRepository(fsClient *firestore.Client) *OtomoRepository {
+	return &OtomoRepository{
+		fsClient: fsClient,
+	}
+}
+
+func (or *OtomoRepository) GetByID(
+	ctx context.Context,
+	id model.UserID,
+) (*model.Otomo, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (or *OtomoRepository) Save(
+	ctx context.Context,
+	otomo *model.Otomo,
+) error {
+	panic("not implemented") // TODO: Implement
+}
