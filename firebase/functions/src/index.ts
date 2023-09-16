db415f62c1782990f71e3c46f09d3df0984303b8
nakar0 <shoutaro425@gmail.com>
2023-09-16 11:43:39 +0900

    :+1: [Firebase] Fixed env structure

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -2,25 +2,40 @@ import {
   onDocumentCreated,
 } from 'firebase-functions/v2/firestore';
 import { Metadata, credentials } from '@grpc/grpc-js';
-import { config } from './config/config';
 import { ChatController } from './controller/chat';
 import { ChatServiceClient } from './protos/chat_service_grpc_pb';
+import { defineSecret, defineString } from 'firebase-functions/params';
 
-const client: ChatServiceClient = new ChatServiceClient(
-  config.otomoServerHost,
-  config.isLocal ? credentials.createInsecure() : credentials.createSsl(),
-);
+const isLocal = process.env.FUNCTIONS_EMULATOR === 'true';
 
-const metadata = new Metadata();
-const auth = Buffer.from(
-  `${config.basicAuthUsername}:${config.basicAuthPassword}`).toString('base64');
-metadata.add('authorization', `basic ${auth}`);
+// Environment variables
+const otomoServerHost = defineString('OTOMO_SERVER_HOST');
 
-const chatController = new ChatController(client, metadata);
+// Secrets
+const otomoBasicAuthUsername = defineSecret('OTOMO_BASIC_AUTH_USERNAME');
+const otomoBasicAuthPassword = defineSecret('OTOMO_BASIC_AUTH_PASSWORD');
+
 
 exports.reply = onDocumentCreated(
-  'versions/1/chats/{userId}/messages/{messageId}',
+  {
+    document: 'versions/1/chats/{userId}/messages/{messageId}',
+    // TODO: asign otomoServerHost
+    secrets: [otomoBasicAuthUsername, otomoBasicAuthPassword],
+  },
   (event) => {
+    const client: ChatServiceClient = new ChatServiceClient(
+      otomoServerHost.value(),
+      isLocal ? credentials.createInsecure() : credentials.createSsl(),
+    );
+
+    const metadata = new Metadata();
+    const auth = Buffer.from(
+      `${otomoBasicAuthUsername.value()}:${otomoBasicAuthPassword.value()}`
+    ).toString('base64');
+    metadata.add('authorization', `basic ${auth}`);
+
+    const chatController = new ChatController(client, metadata);
+
     const snapshot = event.data;
     if (!snapshot) {
       console.log('No data associated with the event');
