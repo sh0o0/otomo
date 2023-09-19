60425623e2be239e717cd5053779fc41eb17726f
nakar0 <shoutaro425@gmail.com>
2023-09-19 14:16:14 +0900

    :+1: [FE] Could launch android

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -3,10 +3,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:otomo/controllers/boundary/id_token.dart';
 import 'package:otomo/configs/app_config.dart';
 import 'package:otomo/configs/injection.dart';
-import 'package:otomo/firebase_options.dart';
+import 'package:otomo/controllers/boundary/id_token.dart';
 import 'package:otomo/tools/logger.dart';
 import 'package:otomo/views/app.dart';
 
@@ -19,7 +18,7 @@ void main() async {
 Future<void> setup() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp();
 
   configureInjection();
 
