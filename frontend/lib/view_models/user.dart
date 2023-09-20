36fd217b16cbc314af9d4bc89343d86d7483bdc2
nakar0 <shoutaro425@gmail.com>
2023-09-20 14:56:03 +0900

    :+1: [FE] Fixed FirebaseAuth to AuthController at authProvider

--- frontend/lib/view_models/user.dart
+++ frontend/lib/view_models/user.dart
@@ -1,26 +1,27 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:otomo/configs/injection.dart';
+import 'package:otomo/controllers/auth.dart';
+import 'package:otomo/entities/user.dart';
 import 'package:otomo/tools/global_state.dart';
 import 'package:otomo/tools/logger.dart';
 
 final authProvider = StateNotifierProvider<AuthNotifier, User?>(
   (ref) => AuthNotifier(
-    getIt<FirebaseAuth>(),
+    getIt<AuthControllerImpl>(),
     getIt<GlobalState>(),
   ),
 );
 
 class AuthNotifier extends StateNotifier<User?> {
-  AuthNotifier(this._firebaseAuth, this._globalState) : super(null) {
-    _firebaseAuth.authStateChanges().listen((user) {
+  AuthNotifier(this._authController, this._globalState) : super(null) {
+    _authController.authStateChanges().listen((user) {
       state = user;
-      _globalState.userId = user?.uid;
-      logger.info('auth state changed. user is ${user?.uid}');
+      _globalState.userId = user?.id;
+      logger.info('auth state changed. user is ${user?.id}');
     });
   }
 
-  final FirebaseAuth _firebaseAuth;
+  final AuthControllerImpl _authController;
   final GlobalState _globalState;
 
   bool get isLoggedIn => state != null;
