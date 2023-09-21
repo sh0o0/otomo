fd7d66c05441b164c1b066752c90c4ffcf5c36d4
nakar0 <shoutaro425@gmail.com>
2023-09-21 11:45:19 +0900

    :+1: [FE] Impl MapState.activePlacesLatLngList

--- frontend/lib/view_models/map.dart
+++ frontend/lib/view_models/map.dart
@@ -2,13 +2,15 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:otomo/configs/injection.dart';
 import 'package:otomo/controllers/location.dart';
+import 'package:otomo/entities/lat_lng.dart';
 import 'package:otomo/entities/place.dart';
 import 'package:otomo/entities/position.dart';
 import 'package:otomo/view_models/chat.dart';
 
 part 'map.freezed.dart';
 
-final mapProvider = StateNotifierProvider.autoDispose<MapNotifier, MapState>((ref) {
+final mapProvider =
+    StateNotifierProvider.autoDispose<MapNotifier, MapState>((ref) {
   final activePlaces =
       ref.watch(chatProvider.select((v) => v.value?.activePlaces));
   final focusPlaceStream = ref.read(chatProvider.notifier).focusPlaceStream;
@@ -20,9 +22,14 @@ final mapProvider = StateNotifierProvider.autoDispose<MapNotifier, MapState>((re
 
 @freezed
 class MapState with _$MapState {
+  const MapState._();
   const factory MapState({
     required List<Place> activePlaces,
   }) = _MapState;
+
+  AppLatLngList get activePlacesLatLngList {
+    return AppLatLngList(activePlaces.map((e) => e.latLng).toList());
+  }
 }
 
 class MapNotifier extends StateNotifier<MapState> {
