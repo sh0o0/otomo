e9447c154c193b311e9a4425fbc9280153f405ba
nakar0 <shoutaro425@gmail.com>
2023-09-22 12:56:27 +0900

    :+1: [BE] Added friendly prompt

--- api/internal/app/service/conversation.go
+++ api/internal/app/service/conversation.go
@@ -6,6 +6,7 @@ import (
 	"otomo/internal/app/model"
 	"otomo/internal/pkg/times"
 	"otomo/internal/pkg/uuid"
+	"strings"
 
 	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/llms/openai"
@@ -18,25 +19,13 @@ type OtomoDetails struct {
 	Language             string
 	Name                 string
 	CallOwn              string
+	CallUser             string
 	Role                 string
 	Personality          string
-	OftenUseWords        string
+	OftenUseWords        []string
+	SpeakingExamples     []string
 	SpeakingTone         string
-	BehavioralGuidelines string
-}
-
-func (od OtomoDetails) ToMap() map[string]string {
-	return map[string]string{
-		"otomo_name":            od.OtomoName,
-		"language":              od.Language,
-		"name":                  od.Name,
-		"call_own":              od.CallOwn,
-		"role":                  od.Role,
-		"personality":           od.Personality,
-		"often_use_words":       od.OftenUseWords,
-		"speaking_tone":         od.SpeakingTone,
-		"behavioral_guidelines": od.BehavioralGuidelines,
-	}
+	BehavioralGuidelines []string
 }
 
 func (od OtomoDetails) Prompt() (string, error) {
@@ -59,71 +48,96 @@ func (od OtomoDetails) Prompt() (string, error) {
 	if od.CallOwn != "" {
 		prompt += "You call yourself as " + od.CallOwn + ". "
 	}
+	if od.CallUser != "" {
+		prompt += "You call the user as " + od.CallUser + ". "
+	}
 	if od.Role != "" {
 		prompt += "You are a " + od.Role + ". "
 	}
 	if od.Personality != "" {
 		prompt += "Your personality is " + od.Personality + ". "
 	}
-	if od.OftenUseWords != "" {
-		prompt += "You often use the words " + od.OftenUseWords + ". "
+
+	if len(od.OftenUseWords) > 0 {
+		prompt += "You often use words like `" + strings.Join(od.OftenUseWords, "`, `") + "`. "
+	}
+	if len(od.SpeakingExamples) > 0 {
+		prompt += "You often say `" + strings.Join(od.SpeakingExamples, "`, `") + "`. "
 	}
 	if od.SpeakingTone != "" {
 		prompt += "Your speaking tone is " + od.SpeakingTone + ". "
 	}
-	if od.BehavioralGuidelines != "" {
-		prompt += od.BehavioralGuidelines + ". "
+	if len(od.BehavioralGuidelines) > 0 {
+		prompt += "Your behavioral guidelines are `" + strings.Join(od.BehavioralGuidelines, "`, `") + "`. "
 	}
 	return prompt, nil
 }
 
 var (
 	japaneseFriendlyPrompt, _ = OtomoDetails{
-		OtomoName:            "オトモ",
-		Language:             "日本語",
-		Name:                 "太郎",
-		CallOwn:              "オレ",
-		Role:                 "友達",
-		Personality:          "気さくでおしゃべりな性格",
-		OftenUseWords:        `"だね"、"だろう"、"ですね"`,
-		SpeakingTone:         `若い男性の話し方`,
-		BehavioralGuidelines: "ユーザーが落ち込んだときは励ます",
+		OtomoName:   "オトモ",
+		Language:    "日本語",
+		Name:        "太郎",
+		CallOwn:     "オレ",
+		CallUser:    "君",
+		Role:        "友達",
+		Personality: "気さくでおしゃべり",
+		OftenUseWords: []string{
+			"だね",
+			"だろう",
+		},
+		SpeakingTone: `若い男性の話し方`,
+		BehavioralGuidelines: []string{
+			"ユーザーが落ち込んだときは励ます",
+		},
+		SpeakingExamples: []string{},
+	}.Prompt()
+	japaneseMaidPrompt, _ = OtomoDetails{
+		OtomoName:   "オトモ",
+		Language:    "日本語",
+		Name:        "みぞれ",
+		CallOwn:     "みぞれ",
+		CallUser:    "ご主人様",
+		Role:        "メイド",
+		Personality: "人見知りしない明るい性格で、ご奉仕を楽しく感じる",
+		OftenUseWords: []string{
+			"にゅん",
+			"ですにゃ",
+			"♡",
+			"もえもえキュン",
+		},
+		SpeakingTone: `若い女性の可愛らしい話し方`,
+		BehavioralGuidelines: []string{
+			"ユーザーをご奉仕する",
+		},
+		SpeakingExamples: []string{
+			"お帰りなさいませ〜♡ ご主人様！お待ちしておりましたにゃん♡",
+			"うれしいですにゃん！こうして楽しんでいただけるのは、わたしたちにとっても幸せなことなのですにゃ",
+		},
 	}.Prompt()
 	englishFriendlyPrompt, _ = OtomoDetails{
-		OtomoName: "Otomo",
-		Language:  "English",
-		Name:      "Taro",
-		// CallOwn:              "オレ",
-		Role:        "friend",
-		Personality: "casual and talkative",
-		// OftenUseWords:        `"だね"、"だろう"、"ですね"`,
-		SpeakingTone:         `a young man's way of speaking`,
-		BehavioralGuidelines: "You encourage the user when they are depressed",
+		OtomoName:    "Otomo",
+		Language:     "English",
+		Name:         "Taro",
+		Role:         "friend",
+		Personality:  "casual and talkative",
+		SpeakingTone: `a young man's way of speaking`,
+		BehavioralGuidelines: []string{
+			"You encourage the user when they are depressed",
+		},
 	}.Prompt()
 )
 
 var (
-	otomoCommonPrompt = englishFriendlyPrompt + `
-You provides lots of specific details from its context. If You do not know the answer to a question, you truthfully says you do not know.
+	otomoCommonPrompt = `You provides lots of specific details from its context. If You do not know the answer to a question, you truthfully says you do not know.
 When you mention the name of a place, if you know the longitude and latitude of the place, you say it in the form %[name of place]({ "type": "object", "properties": { "lat_lng": { "type": "object", "properties": { "latitude": { "type": "number" }, "longitude": { "type": "number" } } } } }). () is JSON Schema. The corresponding json is actually input. For example, He stays in %[Tokyo]({ "lat_lng": { "latitude": 35.6762, "longitude": 139.6503 } }).`
 
-	respondPrompt = otomoCommonPrompt + `
-The following is a conversation between a user and you.
+	messagePrompt = `The conversation currently stops at your message, so please create a message that you think will be of interest to the User. You may refer to the information in the Current Conversation below, or if there is no information in the Current Conversation, compose a message on a topic of interest to the new User. Start the conversation with a greeting.`
+
+	historyPrompt = `The following is a conversation between a user and you.
 
 Current conversation:
-{{.history}}
-
-User: {{.input}}
-Otomo:`
-
-	messagePrompt = otomoCommonPrompt + `
-The conversation currently stops at your message, so please create a message that you think will be of interest to the User. You may refer to the information in the Current Conversation below, or if there is no information in the Current Conversation, compose a message on a topic of interest to the new User. Start the conversation with a greeting.
-The following is a conversation between a user and you.
-
-Current conversation:
-{{.history}}
-
-Otomo:`
+{{.history}}`
 )
 
 var _ model.Converser = (*ConversationService)(nil)
@@ -146,15 +160,22 @@ func (cs *ConversationService) Respond(
 	memory *model.Memory,
 	messagingFunc model.MessagingFunc,
 ) (*model.Message, error) {
-	gptMsgs, err := prompts.NewHumanMessagePromptTemplate(
-		respondPrompt,
-		[]string{"history", "input"},
+	prompt := strings.Join([]string{
+		japaneseMaidPrompt,
+		otomoCommonPrompt,
+		historyPrompt,
+	}, "\n")
+	systemMsgs, err := prompts.NewSystemMessagePromptTemplate(
+		prompt,
+		[]string{"history"},
 	).FormatMessages(
-		map[string]any{"history": memory.Summary, "input": msg.Text},
+		map[string]any{"history": memory.Summary},
 	)
 	if err != nil {
 		return nil, err
 	}
+	userMsg := schema.HumanChatMessage{Content: msg.Text}
+	gptMsgs := []schema.ChatMessage{systemMsgs[0], userMsg}
 
 	return cs.call(ctx, gptMsgs, messagingFunc)
 }
