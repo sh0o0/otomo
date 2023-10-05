53d05228632ab798cee0dea7671493d5377887b2
nakar0 <shoutaro425@gmail.com>
2023-10-05 18:06:04 +0900

    [FE] Fixed analysis

--- frontend/lib/configs/app_themes.dart
+++ frontend/lib/configs/app_themes.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:otomo/configs/app_colors.dart';
