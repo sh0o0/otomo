ec562b9c7b7afbb1aaf5d3c7d88c64f47289f32e
nakar0 <shoutaro425@gmail.com>
2023-10-07 20:23:59 +0900

    [FE] Made prod gcp

---
+++ terraform/environments/prod/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  cloud {
+    organization = "nakar0o0"
+
+    workspaces {
+      name = "otomo"
+    }
+  }
+}
+
+module "otomo" {
+  source                              = "../../modules/otomo"
+  gcp_project_id                      = var.gcp_project_id
+  region                              = var.region
+  load_balancer_name                  = var.load_balancer_name
+  cloud_run_service_name              = var.cloud_run_service_name
+  cloud_run_image_name                = var.cloud_run_image_name
+  api_domain                          = var.api_domain
+  firebase_admin_sdk_credentials_json = var.firebase_admin_sdk_credentials_json
+  basic_auth_pairs                    = var.basic_auth_pairs
+  openai_api_key                      = var.openai_api_key
+}
+
