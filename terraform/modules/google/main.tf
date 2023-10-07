10da0c45bc5a69d618752bf4eaa154bf2a07b4fa
nakar0 <shoutaro425@gmail.com>
2023-10-07 17:52:54 +0900

    [TF] Added google_project_service

--- terraform/modules/google/main.tf
+++ terraform/modules/google/main.tf
@@ -12,5 +12,21 @@ provider "google" {
 }
 
 provider "google-beta" {
-  project               = var.gcp_project_id
+  project = var.gcp_project_id
+}
+
+resource "google_project_service" "default" {
+  project = var.gcp_project_id
+  for_each = toset([
+    "run.googleapis.com",
+    "compute.googleapis.com",
+    "firestore.googleapis.com",
+    "maps-android-backend.googleapis.com",
+    "maps-ios-backend.googleapis.com",
+    "places-backend.googleapis.com",
+    "geocoding-backend.googleapis.com",
+  ])
+  service = each.key
+
+  disable_on_destroy = false
 }
