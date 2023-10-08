47de0f44f746d622f5344c6efdedcd8c638d9f9e
nakar0 <shoutaro425@gmail.com>
2023-10-08 11:22:52 +0900

    [FE] Setted gcp api key headers

--- frontend/lib/controllers/place.dart
+++ frontend/lib/controllers/place.dart
@@ -4,11 +4,20 @@ import 'package:otomo/entities/place_details.dart';
 import 'package:otomo/tools/logger.dart';
 
 class PlaceControllerImpl {
-  PlaceControllerImpl(String apiKey)
-      : _client = Dio(
+  PlaceControllerImpl({
+    required String apiKey,
+    String? androidPackageName,
+    String? androidCertFingerprintSha1,
+    String? iosBundleId,
+  }) : _client = Dio(
           BaseOptions(
             baseUrl: 'https://maps.googleapis.com',
             queryParameters: {'key': apiKey},
+            headers: {
+              'x-android_package_name': androidPackageName ?? '',
+              'x-android_cert_fingerprint': androidCertFingerprintSha1 ?? '',
+              'x-ios-bundle-identifier': iosBundleId ?? '',
+            },
           ),
         );
 
