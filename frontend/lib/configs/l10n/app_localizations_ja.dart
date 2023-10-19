60aac9be2742240c81314dc37bb6399487eed0d9
nakar0 <shoutaro425@gmail.com>
2023-10-19 16:43:43 +0900

    [FE] Generated l10n

---
+++ frontend/lib/configs/l10n/app_localizations_ja.dart
@@ -0,0 +1,9 @@
+import 'app_localizations.dart';
+
+/// The translations for Japanese (`ja`).
+class AppLocalizationsJa extends AppLocalizations {
+  AppLocalizationsJa([String locale = 'ja']) : super(locale);
+
+  @override
+  String get helloWorld => 'こんにちは、世界！';
+}
--- frontend/lib/views/app.dart
+++ frontend/lib/views/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:otomo/configs/app_themes.dart';
+import 'package:otomo/configs/l10n/app_localizations.dart';
 import 'package:otomo/view_models/app.dart';
 import 'package:otomo/view_models/color_theme.dart';
 import 'package:otomo/view_models/router.dart';
