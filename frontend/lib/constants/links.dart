e3667e5108ce90da0a97fc96c7d251420c7e9831
nakar0 <shoutaro425@gmail.com>
2023-10-21 13:54:10 +0900

    [FE] Fixed finishSignInWithEmailLink

--- frontend/lib/constants/links.dart
+++ frontend/lib/constants/links.dart
@@ -2,7 +2,7 @@ final class Links {
   const Links._();
 
   static const terms = 'https://otomo-yeah.studio.site/ja/policies/terms';
-  static const privacyPolicy = 'https://otomo-yeah.studio.site/ja/policies/privacy-policy';
-  static const finishSignInWithEmailLink =
-      'https://otomo-yeah.studio.site/finish-sign-in-with-email-link';
+  static const privacyPolicy =
+      'https://otomo-yeah.studio.site/ja/policies/privacy-policy';
+  static const finishSignInWithEmailLink = 'https://otomo-yeah.studio.site/';
 }
