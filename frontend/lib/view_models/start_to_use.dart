dda2d367ae4487cb0abcd7d6c3afca299d96b9df
nakar0 <shoutaro425@gmail.com>
2023-10-21 17:55:32 +0900

    [FE] Fixed start to use

--- frontend/lib/view_models/start_to_use.dart
+++ frontend/lib/view_models/start_to_use.dart
@@ -7,6 +7,7 @@ import 'package:otomo/controllers/otomo.dart';
 import 'package:otomo/controllers/policies_agreement.dart';
 import 'package:otomo/domains/entities/app_exception.dart';
 import 'package:otomo/domains/entities/date.dart';
+import 'package:otomo/domains/entities/otomo.dart';
 import 'package:otomo/view_models/utils.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -53,29 +54,46 @@ class StartToUse extends _$StartToUse {
 
   Future<void> signOut() => _authController.signOut();
 
-  Future<void> agree() async {
+  Future<void> startToUse(String otomoLanguage) async {
     state = const AsyncValue.loading();
     state = await guard(() async {
-      if (!_value.isAgreed) {
-        throw const AppException(
-          message: 'Must agree',
-          cause: Cause.invalidArgument,
-          domain: Domain.policiesAgreements,
-        );
-      }
-
-      final birthday = _value.birthday;
-      if (birthday == null) {
-        throw const AppException(
-          message: 'Must input birthday',
-          cause: Cause.invalidArgument,
-          domain: Domain.user,
-          field: Field.birthday,
-        );
-      }
-      final userId = readAccount(ref)!.uid;
-      await _agreementController.agree(userId, birthday);
+      await _agree();
+      // Not future intentionally
+      _saveOtomoLanguage(otomoLanguage);
       return _value;
     });
+  }
+
+  Future<void> _agree() async {
+    if (!_value.isAgreed) {
+      throw const AppException(
+        message: 'Must agree',
+        cause: Cause.invalidArgument,
+        domain: Domain.policiesAgreements,
+      );
+    }
+
+    final birthday = _value.birthday;
+    if (birthday == null) {
+      throw const AppException(
+        message: 'Must input birthday',
+        cause: Cause.invalidArgument,
+        domain: Domain.user,
+        field: Field.birthday,
+      );
+    }
+    final userId = readAccount(ref)!.uid;
+    await _agreementController.agree(userId, birthday);
+  }
+
+  Future<void> _saveOtomoLanguage(String otomoLanguage) async {
+    final userId = readAccount(ref)!.uid;
+    final otomo = Otomo(
+      userId: userId,
+      profile: OtomoProfile(
+        language: otomoLanguage,
+      ),
+    );
+    await _otomoController.save(otomo);
   }
 }
