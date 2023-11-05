795f5266fc49560f659fab384394d4fc948b1a52
nakar0 <shoutaro425@gmail.com>
2023-11-05 15:29:28 +0900

    [BE] Added conversation service test

---
+++ api/internal/app/service/conversation_test.go
@@ -0,0 +1,112 @@
+package service
+
+import (
+	"context"
+	_ "embed"
+	"otomo/internal/app/interfaces/svc"
+	"otomo/internal/app/model"
+	"otomo/test/systemtest"
+	"otomo/test/testutil"
+	"testing"
+)
+
+// func newLogMessagingFunc(t *testing.T) model.MessagingFunc {
+// 	return func(_ context.Context, msg *model.MessageChunk) error {
+// 		t.Logf("isLast: %v, Text: %v", msg.IsLast, msg.Text)
+// 		return nil
+// 	}
+// }
+
+func TestSystem_ConversationService_Respond(t *testing.T) {
+	t.Skip(testutil.SystemTest)
+
+	type args struct {
+		ctx  context.Context
+		msg  *model.Message
+		opts svc.ConversationOptions
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantIsErr bool
+	}{
+		{
+			name: "Hello",
+			args: args{
+				ctx: context.Background(),
+				msg: &model.Message{
+					Text: "Hello",
+				},
+			},
+			wantIsErr: false,
+		},
+		{
+			name: "Can you tell me some good hot springs in Japan?",
+			args: args{
+				ctx: context.Background(),
+				msg: &model.Message{
+					Text: "Can you tell me some good hot springs in Japan?",
+				},
+				opts: svc.ConversationOptions{
+					Functions: functions,
+				},
+			},
+			wantIsErr: false,
+		},
+		{
+			name: "Can you tell me a good route of travel in Kyoto?",
+			args: args{
+				ctx: context.Background(),
+				msg: &model.Message{
+					Text: "Can you tell me a good route of travel in Kyoto?",
+				},
+				opts: svc.ConversationOptions{
+					Functions: functions,
+				},
+			},
+			wantIsErr: false,
+		},
+		{
+			name: "Can you plan traveling in Kyoto?",
+			args: args{
+				ctx: context.Background(),
+				msg: &model.Message{
+					Text: "Can you tell me a good route of travel in Kyoto?",
+				},
+				opts: svc.ConversationOptions{
+					Functions: functions,
+				},
+			},
+			wantIsErr: false,
+		},
+		{
+			name: "Tell me details more about Hakone Onsen.",
+			args: args{
+				ctx: context.Background(),
+				msg: &model.Message{
+					Text: "Tell me about Hakone Onsen.",
+				},
+				opts: svc.ConversationOptions{
+					Functions: functions,
+				},
+			},
+			wantIsErr: false,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			cs := &ConversationService{
+				gpt: systemtest.Gpt(),
+			}
+			got, err := cs.Respond(tt.args.ctx, tt.args.msg, tt.args.opts)
+			if (err != nil) != tt.wantIsErr {
+				t.Errorf("ConversationService.Respond() error = %v, wantIsErr %v", err, tt.wantIsErr)
+				return
+			}
+
+			t.Logf("ConversationService.Respond() got = %v", got)
+			t.Fatal(testutil.SystemTest)
+		})
+	}
+}
---
+++ api/test/testutil/constants.go
@@ -0,0 +1,5 @@
+package testutil
+
+var (
+	SystemTest = "SYSTEM TEST"
+)
